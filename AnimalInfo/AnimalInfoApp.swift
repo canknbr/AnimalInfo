38744f9a92e6dfb23aec73083b6217e5eaa399ba
canknbr <cankanbur1@gmail.com>
2023-06-02 09:39:56 +0300

    Initial Commit

---
+++ AnimalInfo/AnimalInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalInfoApp.swift
+//  AnimalInfo
+//
+//  Created by Can Kanbur on 2.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimalInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
